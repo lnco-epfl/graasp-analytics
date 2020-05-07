319877839a8330673bd4a71334de8f8255cbd158
hagoptaminian <hagoptaminian@gmail.com>
2020-05-07 14:26:05 +0300

    feat: add @materialUI and prop-types
    add materialUI dependencies, and prop-types for typechecking
    
    closes #7

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 import Home from './home/Home';
 import Space from './space/Space';
 import Header from './layout/Header';
 import Footer from './layout/Footer';
 
+const useStyles = makeStyles(() => ({
+  main: {},
+}));
+
 function App() {
+  const classes = useStyles();
   return (
     <Router>
       <Header />
-      <Switch>
-        <Route path="/spaces/:id">
-          <Space />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
+      <main className={classes.main}>
+        <Switch>
+          <Route path="/spaces/:id">
+            <Space />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </main>
       <Footer />
     </Router>
   );
